f199fc839bda99d1ebec448529f87bf7c7168358
Kimon Meier <kimon.meier@gmail.com>
2025-06-19 11:26:51 +0200

    Add LinieTrainRepository to the service configuration
    Introduced a new repository, LinieTrainRepository, for handling LinieTrain entities. Updated the dependency injection setup to include this repository in the service container. This improves extensibility and supports future database operations for LinieTrain.

---
+++ Streckenbuch.Server/Data/Repositories/LinieTrainRepository.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Streckenbuch.Server.Data.Entities.Linien;
+using Streckenbuch.Shared.Data.Repositories;
+
+namespace Streckenbuch.Server.Data.Repositories;
+
+public class LinieTrainRepository : GenericRepository<LinieTrain>
+{
+    public LinieTrainRepository(DbContext dbContext) : base(dbContext)
+    {
+    }
+}
