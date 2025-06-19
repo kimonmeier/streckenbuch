0d7fd4da706073c3aba2d9f7e802ed7afd81eb54
Kimon Meier <kimon.meier@gmail.com>
2025-06-19 11:23:40 +0200

    Add LinieTrain entity and related database migration
    Introduced the LinieTrain entity with properties for TrainNumber and its relationship with Linie. Added a new database migration to create the LinieTrain table, configure indices, and define referential integrity constraints. Updated the application model snapshot to reflect these changes.

---
+++ Streckenbuch.Server/Data/Configuration/LinieTrainConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streckenbuch.Server.Data.Entities.Linien;
+
+namespace Streckenbuch.Server.Data.Configuration;
+
+public class LinieTrainConfiguration : IEntityTypeConfiguration<LinieTrain>
+{
+    public void Configure(EntityTypeBuilder<LinieTrain> builder)
+    {
+        builder
+            .ToTable(nameof(LinieTrain));
+
+        builder
+            .HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder
+            .HasOne(x => x.Linie)
+            .WithOne()
+            .HasForeignKey<LinieTrain>(x => x.LinieId);
+
+        builder
+            .HasIndex(x => x.TrainNumber);
+    }
+}
