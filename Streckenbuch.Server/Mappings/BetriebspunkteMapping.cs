78c43c14332adde7792000b84b872d8b77093ab1
Kimon Meier <kimon.meier@gmail.com>
2025-06-19 22:51:24 +0200

    Add MikuId field to Betriebspunkte entities and UI
    Introduced a new MikuId field to Betriebspunkte across the client, server, and shared layers. Updated the database mapping, protobuf definitions, UI components, and form handling logic to support this new field. This ensures consistent handling and display of MikuId within the application.

--- Streckenbuch.Server/Mappings/BetriebspunkteMapping.cs
+++ Streckenbuch.Server/Mappings/BetriebspunkteMapping.cs
@@ -13,6 +13,7 @@ public class BetriebspunkteMapping : IMap<Betriebspunkt, BetriebspunktProto>
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.Kommentar, opt => opt.MapFrom(src => src.Kommentar))
-            .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location));
+            .ForMember(dest => dest.Location, opt => opt.MapFrom(src => src.Location))
+            .ForMember(dest => dest.MikuId, opt => opt.MapFrom(src => src.MikuId));
     }
 }
