3e90e6cf3738cf8694d11d526cb888f4fb219f83
Kimon Meier <kimon.meier@gmail.com>
2025-06-19 11:41:47 +0200

    Add train number linking functionality to LinienService
    Implemented methods to link and retrieve train numbers with Linien entities. Updated protobuf definitions and extended LinieTrainRepository for train number lookups. Refactored constructor and cleaned up code formatting where necessary.

--- Streckenbuch.Server/Data/Repositories/LinieTrainRepository.cs
+++ Streckenbuch.Server/Data/Repositories/LinieTrainRepository.cs
@@ -9,4 +9,10 @@ public class LinieTrainRepository : GenericRepository<LinieTrain>
     public LinieTrainRepository(DbContext dbContext) : base(dbContext)
     {
     }
+
+
+    public Task<LinieTrain?> FindByTrainNumberAsync(int trainNumber)
+    {
+        return Entities.SingleOrDefaultAsync(x => x.TrainNumber == trainNumber);
+    }
 }
